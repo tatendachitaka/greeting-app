e2eb18b12261cc78770fb4b178ba90dc943b0e0e
Tatenda <tchitak1@stu.vistula.edu.pl>
2025-05-19 14:36:19 +0200

    Initial commit - Spring Boot greeting app

---
+++ greeting-app/src/test/java/com/example/greeting/app/GreetingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greeting.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
